80901afc6b60de2740cfbcd03b8c403669a6b079
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-10-03 14:20:36 -0300

    Rename data structures

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from functools import cmp_to_key
 
-MAP_OF_PROJECTS = {};
-MAP_OF_STUDENTS = {};
+PROJECTS_STUDENTS_MAP = {};
+STUDENTS_PREFERRED_PROJECT_MAP = {};
 
 class Project(object):
     def __init__(self, id, seats, minimum_grade):
@@ -31,19 +31,19 @@ def read_file_and_create_sets():
         if PROJECTS_START_LINE <= count and count <= PROJECTS_END_LINE:
             project_data = line.strip().split()
             project = Project(project_data[0], project_data[1], project_data[2])
-            MAP_OF_PROJECTS[project] = []
+            PROJECTS_STUDENTS_MAP[project] = []
 
         # Create students
         if STUDENTS_START_LINE <= count and count <= STUDENTS_END_LINE:
             student_data = line.strip().split()
             student = Student(student_data[0], student_data[4])
             student_projects_ids = [student_data[1], student_data[2], student_data[3]]
-            MAP_OF_STUDENTS[student] = []
+            STUDENTS_PREFERRED_PROJECT_MAP[student] = []
 
             for student_project_id in student_projects_ids:
-                matches = (project for project in MAP_OF_PROJECTS.keys() if project.id == student_project_id)
+                matches = (project for project in PROJECTS_STUDENTS_MAP.keys() if project.id == student_project_id)
                 project = next(matches)
-                MAP_OF_STUDENTS[student] = MAP_OF_STUDENTS[student] + [project]
+                STUDENTS_PREFERRED_PROJECT_MAP[student] = STUDENTS_PREFERRED_PROJECT_MAP[student] + [project]
 
         count += 1
 
@@ -54,8 +54,8 @@ def student_key(item):
 # busca todos os alunos que já tem um projeto definido
 def all_project_students():
     students = []
-    for project in MAP_OF_PROJECTS.keys():
-        students = students + MAP_OF_PROJECTS[project]
+    for project in PROJECTS_STUDENTS_MAP.keys():
+        students = students + PROJECTS_STUDENTS_MAP[project]
 
     return students
 
@@ -65,13 +65,13 @@ def remove_student(students_to_remove, project):
     for student in students_to_remove:
         new_projects = []
         first_removed = False
-        for project_to_validate in MAP_OF_STUDENTS[student]:
+        for project_to_validate in STUDENTS_PREFERRED_PROJECT_MAP[student]:
             if project_to_validate == project and first_removed == False:
                 first_removed = True
             else:
                 new_projects = new_projects + [project_to_validate]
 
-        MAP_OF_STUDENTS[student] = new_projects
+        STUDENTS_PREFERRED_PROJECT_MAP[student] = new_projects
 
 # execução do algoritmo
 def run():
@@ -79,37 +79,37 @@ def run():
     students_without_project = []
     count = 0
 
-    while len(students_waitlist) < len(MAP_OF_STUDENTS.keys()):
+    while len(students_waitlist) < len(STUDENTS_PREFERRED_PROJECT_MAP.keys()):
         print('--------------------------------------')
         print(f'---- INICIO DA ITERAÇÃ0 {count} (PROJETO: LISTA DE ALUNOS): ----')
 
         print_all()
 
-        for student in MAP_OF_STUDENTS.keys():
+        for student in STUDENTS_PREFERRED_PROJECT_MAP.keys():
             if student not in students_waitlist:
-                if len(MAP_OF_STUDENTS[student]) == 0:
+                if len(STUDENTS_PREFERRED_PROJECT_MAP[student]) == 0:
                     students_without_project = students_without_project + [student]
                     continue
 
-                project = MAP_OF_STUDENTS[student][0]
-                MAP_OF_PROJECTS[project] = MAP_OF_PROJECTS[project] + [student]
+                project = STUDENTS_PREFERRED_PROJECT_MAP[student][0]
+                PROJECTS_STUDENTS_MAP[project] = PROJECTS_STUDENTS_MAP[project] + [student]
 
         # remove os alunos que não tem nota para o projeto
-        for project in MAP_OF_PROJECTS.keys():
-            list_of_students = MAP_OF_PROJECTS[project]
-            fitered_students = list(filter(lambda s: project.minimum_grade <= s.grade, MAP_OF_PROJECTS[project]))
-            MAP_OF_PROJECTS[project] = sorted(fitered_students, key=student_key)
+        for project in PROJECTS_STUDENTS_MAP.keys():
+            list_of_students = PROJECTS_STUDENTS_MAP[project]
+            fitered_students = list(filter(lambda s: project.minimum_grade <= s.grade, PROJECTS_STUDENTS_MAP[project]))
+            PROJECTS_STUDENTS_MAP[project] = sorted(fitered_students, key=student_key)
             remove_student(list(set(list_of_students) - set(fitered_students)), project)
 
-        projects_with_more_student = list(filter(lambda p: int(p.seats) < len(MAP_OF_PROJECTS[p]), MAP_OF_PROJECTS.keys()))
+        projects_with_more_student = list(filter(lambda p: int(p.seats) < len(PROJECTS_STUDENTS_MAP[p]), PROJECTS_STUDENTS_MAP.keys()))
 
         # remove os alunos excedentes do projeto
         for project in projects_with_more_student:
             students_to_remove = []
-            new_students_list = MAP_OF_PROJECTS[project]
-            for i in range(len(MAP_OF_PROJECTS[project]) - int(project.seats)):
+            new_students_list = PROJECTS_STUDENTS_MAP[project]
+            for i in range(len(PROJECTS_STUDENTS_MAP[project]) - int(project.seats)):
                 students_to_remove = students_to_remove + [new_students_list.pop(0)]
-            MAP_OF_PROJECTS[project] = new_students_list
+            PROJECTS_STUDENTS_MAP[project] = new_students_list
             remove_student(students_to_remove, project)
 
         students_waitlist = students_without_project + all_project_students()
@@ -132,8 +132,8 @@ def list_students(students):
     return students_ids
 
 def print_all():
-    for key in MAP_OF_PROJECTS.keys():
-        print(f"{key.id} : {list_students(MAP_OF_PROJECTS[key])}")
+    for key in PROJECTS_STUDENTS_MAP.keys():
+        print(f"{key.id} : {list_students(PROJECTS_STUDENTS_MAP[key])}")
 
 def main():
     read_file_and_create_sets()
